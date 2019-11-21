6cd11de1f6e4cadf78edad702b4bab5bf6fb4836
vdecaro <valeriodc95@hotmail.it>
2019-11-21 19:17:36 +0100

    user-service package removed

--- user-service/extensions.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from celery import Celery
-celery = Celery()
-
-from flask_sqlalchemy import SQLAlchemy
-db = SQLAlchemy()
--- user-service/tasks.py
+++
@@ -1,8 +0,0 @@
-from flask import current_app
-
-from gateway.extensions import celery
-
-
-@celery.task
-def request_signup():
-    return 200
